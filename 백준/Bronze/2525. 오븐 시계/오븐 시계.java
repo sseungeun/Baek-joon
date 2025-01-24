63b97f77c6998ee3827bc0e9c07e5f2e3f3fb341
Seungeun Lee <153576619+sseungeun@users.noreply.github.com>
2025-01-25 01:44:07 +0900

    [Bronze III] Title: 오븐 시계, Time: 188 ms, Memory: 18420 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+		Scanner in=new Scanner(System.in);
+		int A=in.nextInt();
+		int B=in.nextInt();
+		int C=in.nextInt();
+		
+		int min=A*60+B;
+		min=min+C;
+		
+		int hour=(min/60)%24;
+		int minute=min%60;
+		
+		System.out.println(hour+" "+minute);
+		
+    }
+}
