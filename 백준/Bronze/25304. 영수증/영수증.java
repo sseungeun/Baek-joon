a8e1e22fade8804372eb7da50d34bee4a0c1b315
Seungeun Lee <153576619+sseungeun@users.noreply.github.com>
2025-01-26 17:40:33 +0900

    Update 영수증.java

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -3,14 +3,14 @@ public class Main{
     public static void main(String args[]){
     	Scanner sc=new Scanner(System.in);
     	
-    	int c=sc.nextInt();
-    	int n=sc.nextInt();
+    	int c=sc.nextInt(); // 총 금액
+    	int n=sc.nextInt(); //종류의 수
     	
     	int sum=0;
     	
     	for(int i=0;i<n;i++) {
-    		int a=sc.nextInt();
-    		int b=sc.nextInt();
+    		int a=sc.nextInt(); //각 물건의 가격
+    		int b=sc.nextInt(); //각 물건의 개수
     		
     		sum+=a*b;
     	}
